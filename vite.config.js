798479e1aaec5c38ec77b4aacc80a9fbd29c0da7
sahilw25 <walawalkarsahil25@gmail.com>
2025-11-15 05:02:32 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 export default defineConfig({
   plugins: [react()],
-  base: '/my-react-tailwind-app/'  // <-- IMPORTANT
+  base: '/my-react-tailwind-app/' // <-- ADD THIS
 })
-=======
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> e566cc65a93dcb168993d60dbd26b74464503af5
